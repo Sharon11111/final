7a703c5376b629ee784972f5eea34ad17c1fc804
Neeka <131469677+Neeka1214@users.noreply.github.com>
2023-05-26 03:41:28 +0800

    Update index.php

--- project/index.php
+++ project/index.php
@@ -6,29 +6,29 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link rel="stylesheet" href="login.css">
+    <link rel="stylesheet" href="login11.css">
 </head>
 
 <body>
+<a href="Aboutme_login.php" class="de" >回主頁</a> 
 <form action="login.php" method="post">
-       <h2>Login</h2>
+      
         <?php if (isset($_GET['error'])) {?>
-            <p class="error"><?php echo $_GET['error']; ?></p>    
+            <p class="error"><?php echo $_GET['error']; ?></p>
         <?php } ?>
-<div class=three> 
-        
-       
+<div class=three>
+<h2>登入</h2>
         <label>身分證</label>
         <br>
-        <input type="password" name="password" placeholder="Password">
+        <input type="text" name="identification_number" placeholder="ID number">
         <br>
         <label>密碼</label>
         <br>
         <input type="password" name="password" placeholder="Password">
         <br>
-        <button type="submit">Login</button>
+        <button type="submit">登入</button>
         <br>
-        <a href="Register.php"  class="ca" >Have a new account.</a>
+        <a href="Register.php"  class="ca" >創建新帳號</a>
         </div>
     </form>
 </body>
