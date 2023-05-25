aa2f9f78ad19066f5661588f9ba280aaddcbc4cc
Neeka <131469677+Neeka1214@users.noreply.github.com>
2023-05-26 03:54:02 +0800

    Update Register.php

--- project/Register.php
+++ project/Register.php
@@ -12,6 +12,8 @@
    
 <form action="signup-check.php" method="post">
 
+    <h2>註冊</h2>
+<div class =three>
     <?php if (isset($_GET['error'])) {?>
      <p class="error"><?php echo $_GET['error']; ?></p>    
      <?php } ?>
@@ -19,8 +21,7 @@
      <?php if (isset($_GET['success'])) {?>
      <p class="success"><?php echo $_GET['success']; ?></p>    
     <?php } ?>
-<div class =three>
-        <h2>註冊</h2>
+       
         <br>
         <label>名字</label>
         <br>
